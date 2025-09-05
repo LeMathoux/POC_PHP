5926c6a7387a374dc178f797a4b5fe3ddd94f410
LeMathoux <123992331+LeMathoux@users.noreply.github.com>
2025-09-05 20:16:00 +0200

    set Media to AbstractClass and continue CRUD funct

--- Models/Book.php
+++ Models/Book.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once './ConnexionBD.php';
+include_once './Media.php';
 
 class Book extends Media{
 
@@ -24,6 +25,11 @@ class Book extends Media{
         return $this->pageNumber;
     }
     
+    public function getMediaId(): ?int
+    {
+        return parent::getId();
+    }
+
     public function setId(int $id)
     {
         $this->id = $id;
@@ -33,4 +39,159 @@ class Book extends Media{
     {
         $this->pageNumber = $pageNumber;
     }
+
+    public function setMediaId(int $mediaId)
+    {
+        parent::setId($mediaId);
+    }
+
+    public static function getBookById(int $id){
+        try{
+
+            $connexion = connexion();
+            $request = $connexion->prepare("SELECT book.id, title, author, available, page_number, media_id FROM book JOIN media ON media.id = book.media_id WHERE book.id=:id;");
+
+            $request->bindParam(":id",$id);
+
+            $request->execute();
+
+            $bookInfo = $request->fetch(PDO::FETCH_ASSOC);
+
+            if(!empty($bookInfo)){
+                $bookObject = new Book($bookInfo['title'], $bookInfo['author'], $bookInfo['available'], $bookInfo['page_number'], $bookInfo['media_id'], $bookInfo['id']);
+            }else{
+                $bookObject = null;
+            }
+
+            return $bookObject;
+
+        }catch(PDOException $e){
+            die('Book Get By Id Error : '.$e);
+        }
+    }
+
+    public static function getAllBooks(){
+        try{
+            $booksList = [];
+
+            $connexion = connexion();
+            $request = $connexion->prepare("SELECT book.id, title, author, available, page_number, media_id FROM book JOIN media ON media.id = book.media_id;");
+
+            $request->execute();
+
+            $booksInfo = $request->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach($booksInfo as $bookInfo){
+                $bookObject = new Book($bookInfo['title'], $bookInfo['author'], $bookInfo['available'], $bookInfo['page_number'], $bookInfo['media_id'], $bookInfo['id']);
+                array_push($booksList,$bookObject);
+            }
+            
+            return $booksList;
+
+        }catch(PDOException $e){
+            die('Book Get All Error : '.$e);
+        }
+    }
+
+    public function addNewBook()
+    {
+        try{
+            $connexion = connexion();
+            $request = $connexion->prepare("INSERT INTO media (title, author, available) VALUES(:title, :author, :availble);");
+
+            $title = $this->getTitle();
+            $author = $this->getAuthor();
+            $available = $this->getAvailable();
+            $pageNumber = $this->getPageNumber();
+
+
+            $request->bindParam(":title",$title);
+            $request->bindParam(":author",$author);
+            $request->bindParam(":availble",$available);
+
+            $request->execute();
+
+            $getIdInserted = $connexion->lastInsertId();
+            $this->setMediaId($getIdInserted);
+            $mediaId = $this->getMediaId();
+
+            $request = $connexion->prepare("INSERT INTO book (page_number, media_id) VALUES(:page_number, :media_id);");
+
+            $request->bindParam(":page_number",$pageNumber);
+            $request->bindParam(":media_id",$mediaId);
+
+            $request->execute();
+
+            $getIdInserted = $connexion->lastInsertId();
+            $this->setId($getIdInserted);
+
+            return $this;
+
+        }catch(PDOException $e){
+            die('Book Add Error : '.$e);
+        }
+    }
+
+    public function updateBook(){
+        try{
+            $connexion = connexion();
+            $request = $connexion->prepare("UPDATE media SET title=:title, author=:author, available=:available WHERE id=:mediaId;");
+
+            $id = $this->getId();
+            $mediaId = $this->getMediaId();
+            $title = $this->getTitle();
+            $author = $this->getAuthor();
+            $available = $this->getAvailable();
+            $pageNumber = $this->getPageNumber();
+
+            $request->bindParam(":mediaId",$mediaId);
+            $request->bindParam(":title",$title);
+            $request->bindParam(":author",$author);
+            $request->bindParam(":available",$available);
+
+            $request->execute();
+
+            $request = $connexion->prepare("UPDATE book SET page_number=:page_number WHERE id=:id;");
+
+            $request->bindParam(":id",$id);
+            $request->bindParam(":page_number",$$pageNumber);
+            
+            $request->execute();
+
+            return $this;
+
+        }catch(PDOException $e){
+            die('Book Update Error : '.$e);
+        }
+    }
+
+    public function removeBook(){
+        try{
+            $connexion = connexion();
+            
+            $id = $this->getId();
+            $mediaId = $this->getMediaId();
+
+            $request = $connexion->prepare("DELETE FROM book WHERE id=:id;");
+
+            $request->bindParam(":id",$id);
+            
+            $request->execute();
+
+            $request = $connexion->prepare("DELETE FROM media WHERE id=:mediaId;");
+
+            $request->bindParam(":mediaId",$mediaId);
+
+            $request->execute();
+
+            return true;
+
+        }catch(PDOException $e){
+            die('Book Delete Current Object Error : '.$e);
+        }
+    }
 }
+
+$book = new Book('test','test auh',true,12,null,null);
+var_dump(Book::getAllBooks());
+var_dump(Book::getBookById(3));
--- Models/Movie.php
+++ Models/Movie.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once './ConnexionBD.php';
+include_once './Media.php';
 
 enum Genre:string{
     case Policier = "Policier";
@@ -36,6 +37,11 @@ class Movie extends Media{
     {
         return $this->genre;
     }
+        
+    public function getMediaId(): ?int
+    {
+        return parent::getId();
+    }
 
     public function setId(int $id)
     {
@@ -49,5 +55,162 @@ class Movie extends Media{
 
     public function setGenre(Genre $genre){
         $this->genre = $genre;
+    }
+    
+    public function setMediaId(int $mediaId)
+    {
+        parent::setId($mediaId);
+    }
+
+    
+    public static function getMovieById(int $id){
+        try{
+
+            $connexion = connexion();
+            $request = $connexion->prepare("SELECT movie.id, title, author, available, duration, genre, media_id FROM movie JOIN media ON media.id = movie.media_id WHERE movie.id=:id;");
+
+            $request->bindParam(":id",$id);
+
+            $request->execute();
+
+            $movieInfo = $request->fetch(PDO::FETCH_ASSOC);
+
+            if(!empty($movieInfo)){
+                $genreObject = Genre::from($movieInfo["genre"]);
+                $movieObject = new Movie($movieInfo['title'], $movieInfo['author'], $movieInfo['available'], $movieInfo['duration'], $genreObject, $movieInfo['media_id'], $movieInfo['id']);
+            }else{
+                $movieObject = null;
+            }
+
+            return $movieObject;
+
+        }catch(PDOException $e){
+            die('Movie Get By Id Error : '.$e);
+        }
+    }
+
+    public static function getAllMovies(){
+        try{
+            $moviesList = [];
+
+            $connexion = connexion();
+            $request = $connexion->prepare("SELECT movie.id, title, author, available, duration, genre, media_id FROM movie JOIN media ON media.id = movie.media_id;");
+
+            $request->execute();
+
+            $moviesInfo = $request->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach($moviesInfo as $movieInfo){
+                $genreObject = Genre::from($movieInfo["genre"]);
+                $movieObject = new Movie($movieInfo['title'], $movieInfo['author'], $movieInfo['available'], $movieInfo['duration'], $genreObject, $movieInfo['media_id'], $movieInfo['id']);
+                array_push($moviesList,$movieObject);
+            }
+            
+            return $moviesList;
+
+        }catch(PDOException $e){
+            die('Movie Get All Error : '.$e);
+        }
+    }
+
+    public function addNewMovie()
+    {
+        try{
+            $connexion = connexion();
+            $request = $connexion->prepare("INSERT INTO media (title, author, available) VALUES(:title, :author, :availble);");
+
+            $title = $this->getTitle();
+            $author = $this->getAuthor();
+            $available = $this->getAvailable();
+            $duration = $this->getDuration();
+            $genre = $this->getGenre()->value;
+
+
+            $request->bindParam(":title",$title);
+            $request->bindParam(":author",$author);
+            $request->bindParam(":availble",$available);
+
+            $request->execute();
+
+            $getIdInserted = $connexion->lastInsertId();
+            $this->setMediaId($getIdInserted);
+            $mediaId = $this->getMediaId();
+
+            $request = $connexion->prepare("INSERT INTO movie (duration, genre, media_id) VALUES(:duration, :genre, :media_id);");
+
+            $request->bindParam(":duration",$duration);
+            $request->bindParam(":genre",$genre);
+            $request->bindParam(":media_id",$mediaId);
+
+            $request->execute();
+
+            $getIdInserted = $connexion->lastInsertId();
+            $this->setId($getIdInserted);
+
+            return $this;
+
+        }catch(PDOException $e){
+            die('Movie Add Error : '.$e);
+        }
+    }
+
+    public function updateBook(){
+        try{
+            $connexion = connexion();
+            $request = $connexion->prepare("UPDATE media SET title=:title, author=:author, available=:available WHERE id=:mediaId;");
+
+            $title = $this->getTitle();
+            $author = $this->getAuthor();
+            $available = $this->getAvailable();
+            $duration = $this->getDuration();
+            $genre = $this->getGenre();
+            $mediaId = $this->getMediaId();
+
+            $request->bindParam(":mediaId",$mediaId);
+            $request->bindParam(":title",$title);
+            $request->bindParam(":author",$author);
+            $request->bindParam(":available",$available);
+
+            $request->execute();
+
+            $request = $connexion->prepare("UPDATE movie SET duration=:duration, genre=:genre  WHERE id=:id;");
+
+            $request->bindParam(":id",$id);
+            $request->bindParam(":duration",$duration);
+            $request->bindParam(":genre",$genre);
+            
+            $request->execute();
+
+            return $this;
+
+        }catch(PDOException $e){
+            die('Movie Update Error : '.$e);
+        }
+    }
+
+    public function removeMovie(){
+        try{
+            $connexion = connexion();
+            
+            $id = $this->getId();
+            $mediaId = $this->getMediaId();
+
+            $request = $connexion->prepare("DELETE FROM movie WHERE id=:id;");
+
+            $request->bindParam(":id",$id);
+            
+            $request->execute();
+
+            $request = $connexion->prepare("DELETE FROM media WHERE id=:mediaId;");
+
+            $request->bindParam(":mediaId",$mediaId);
+
+            $request->execute();
+
+            return true;
+
+        }catch(PDOException $e){
+            die('Movie Delete Current Object Error : '.$e);
+        }
     }
 }
